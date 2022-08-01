9ebc8f1b064e6f8b3351de3f7bf5680220d67d31
katacoder48 <katacoder48@gmail.com>
2022-08-01 18:17:54 +0900

    update baobab deployment

--- deploy/cypress/witnet.ts
+++ deploy/cypress/witnet.ts
@@ -1,5 +1,7 @@
-import {DeployFunction} from "hardhat-deploy/types";
-import {HardhatRuntimeEnvironment} from "hardhat/types";
+import { DeployFunction } from "hardhat-deploy/types";
+import { HardhatRuntimeEnvironment } from "hardhat/types";
+import { PriceOracle, WitnetExternalOracle } from "../../types";
+import { doExecute } from "../../tasks/utils";
 
 const deployFunction: DeployFunction = async function ({
                                                          ethers,
@@ -27,6 +29,55 @@ const deployFunction: DeployFunction = async function ({
     },
     log: true,
   });
+
+  const oracle = (await ethers.getContract("PriceOracle")) as PriceOracle;
+  const witnet = (await ethers.getContract(
+    "WitnetExternalOracle"
+  )) as WitnetExternalOracle;
+
+  for (const klay of [
+    ethers.constants.AddressZero, // address(0)
+    "0xe4f05A66Ec68B54A58B17c22107b02e0232cC817", // claimswap's wklay
+    "0x5819b6af194a78511c79c85ea68d2377a7e9335f", // definix's wklay
+    "0x2ff5f6de2287ca3075232127277e53519a77947c", // pala's wklay
+    "0xf01f433268c277535743231c95c8e46783746d30", // roundrobin's wklay
+    "0x8dfbb066e2881c85749cce3d9ea5c7f1335b46ae", // ufo's wklay
+  ]) {
+    const value = ethers.utils.keccak256(
+      ethers.utils.toUtf8Bytes("Price-KLAY/USD-6")
+    );
+    await doExecute(witnet.register(klay, value, 6));
+  }
+  await doExecute(
+    oracle.registerTokenOracle(ethers.constants.AddressZero, witnet.address)
+  );
+  await doExecute(
+    oracle.registerBaseToken(ethers.constants.AddressZero)
+  )
+
+  for (const usdt of [
+    "0xcee8faf64bb97a73bb51e115aa89c17ffa8dd167",
+  ]) {
+    const value = ethers.utils.keccak256(
+      ethers.utils.toUtf8Bytes("Price-USDT/USD-6")
+    );
+
+    await doExecute(witnet.register(usdt, value, 6));
+    await doExecute(oracle.registerTokenOracle(usdt, witnet.address));
+    await doExecute(oracle.registerBaseToken(usdt));
+  }
+
+  for (const ksp of [
+    "0xc6a2ad8cc6e4a7e08fc37cc5954be07d499e7654",
+  ]) {
+    const value = ethers.utils.keccak256(
+      ethers.utils.toUtf8Bytes("Price-KSP/USD-6")
+    );
+
+    await doExecute(witnet.register(ksp, value, 6));
+    await doExecute(oracle.registerTokenOracle(ksp, witnet.address));
+    await doExecute(oracle.registerBaseToken(ksp));
+  }
 };
 export default deployFunction;
 
